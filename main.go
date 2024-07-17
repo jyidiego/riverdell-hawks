e3f36951cacbaf15dbaaa48a87bfafb68fe99bac
John Yi <johnyi@ShadowMacbook.attlocal.net>
2024-07-17 09:40:23 -0500

    Initial commit for harness lab

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type club struct {
+	ID       int    `json:"id"`
+	TeamName string `json:"teamname"`
+	Flight   string `json:"flight"`
+}
+
+var teams = []club{
+	{ID: 0, TeamName: "RiverDell Hawks", Flight: "U12A"},
+	{ID: 1, TeamName: "Maroons", Flight: "U12A"},
+	{ID: 2, TeamName: "WC", Flight: "U12A"},
+}
+
+func getTeams(c *gin.Context) {
+	c.IndentedJSON(http.StatusAccepted, teams)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/teams", getTeams)
+
+	router.Run("localhost:8080")
+}
